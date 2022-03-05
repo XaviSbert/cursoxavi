c8611e9b5deb3cbbe1f5f5ef88b043ce2c6edb4e
Xavi Sbert <xavisc999@gmail.com>
2022-03-05 19:44:18 +0100

    paso 22

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import imageRickMorty from './img/rick-morty.png';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+    
+    setCharacters(characterApi.results);
+    // console.log(api);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='title'>Rick & Morty</h1>
+        <img src={imageRickMorty} alt="Rick & Morty" className='img-home'/>
+        <button onClick={reqApi} className="btn-search">Buscar personajes</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
